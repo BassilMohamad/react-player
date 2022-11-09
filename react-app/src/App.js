dc7927624de771b44ab96bd66f17c4786809d947
bassil <bassiljarrah2@gmail.com>
2022-11-10 00:05:04 +0300

    react-player

---
+++ react-app/src/App.js
@@ -0,0 +1,45 @@
+import React,{useState,useRef} from "react";
+//import styles
+import "./styles/app.scss";
+//import Componants
+import Song from "./componants/Song";
+import Player from "./componants/Player";
+import Library from "./componants/Library";
+import Navbar from "./componants/Navbar";
+//importing utill 
+import data from "./utill";
+ 
+function App() {
+  //run Normal js
+  const [songs,setSongs]=useState(data);
+  const [currentSong,setCurrentSong]=useState(songs[0]);
+  const [isPlaying,setIsPlaying]=useState(false);
+  const [active,setActive] = useState(false);
+  //Ref
+  const audioRef=useRef(null);
+  return (
+    <div className={`App ${active ?  "library-active" : "" }`}>
+      <Navbar active={active} setActive={setActive}/>
+      <Song currentSong={currentSong} />
+      <Player 
+      currentSong={currentSong}
+      isPlaying={isPlaying}
+      setIsPlaying={setIsPlaying}
+      audioRef={audioRef}
+      songs={songs}
+      setCurrentSong={setCurrentSong}
+      setSongs={setSongs}
+      />
+      <Library 
+      songs={songs}
+      setCurrentSong={setCurrentSong}
+      setSongs={setSongs}
+      isPlaying={isPlaying}
+      audioRef={audioRef}
+      active={active}
+      />
+    </div>
+  );
+}
+
+export default App;
